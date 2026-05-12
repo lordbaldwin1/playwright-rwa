1a6804c3cfb50f791bbbc6dfe65be990cbfc0673
Zachary Springer <springerczachary@gmail.com>
2026-05-12 00:19:23 -0700

    working on bankaccount tests

---
+++ playwright/pages/BankAccountsPage.ts
@@ -0,0 +1,14 @@
+import { Page } from "@playwright/test";
+
+
+export class BankAccountsPage {
+  private readonly page: Page;
+
+  constructor(page: Page) {
+    this.page = page;
+  }
+
+  async goto() {
+    await this.page.goto("/bankaccounts");
+  }
+}
