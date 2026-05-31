3cb0e4572dff55bd2f7c100e12f5cfb699904709
Zachary Springer <springerzman@gmail.com>
2026-05-30 18:45:45 -0700

    more graphql work

--- playwright/tests/api/gql-test.spec.ts
+++ playwright/tests/api/gql-test.spec.ts
@@ -2,6 +2,7 @@ import { BankAccount } from "models";
 import { config } from "../../config";
 import { expect, test } from "../../fixtures/api";
 import { L } from "vitest/dist/chunks/reporters.d.BFLkQcL6";
+import faker from "@faker-js/faker";
 
 const API_GRAPHQL = `${config.BACKEND_URL}/graphql`;
 
@@ -54,7 +55,7 @@ test.describe("graphql playground", () => {
   }) => {
     const res = await request.post(API_GRAPHQL, {
       data: {
-        query: `query {
+        query: `query listBankAccount {
         listBankAccount {
           id
           bankName
@@ -87,16 +88,17 @@ test.describe("graphql playground", () => {
 
     const res = await request.post(API_GRAPHQL, {
       data: {
-        query: `mutation {
-          createBankAccount(
-            bankName: "${bankToAdd.bankName}"
-            accountNumber: "${bankToAdd.accountNumber}"
-            routingNumber: "${bankToAdd.routingNumber}"
-          ) {
+        query: `mutation createBankAccount ($bankName: String!, $accountNumber: String!, $routingNumber: String!) {
+          createBankAccount(bankName: $bankName, accountNumber: $accountNumber, routingNumber: $routingNumber) {
             id
-            userId  
+            userId
           }
         }`,
+        variables: {
+          bankName: bankToAdd.bankName,
+          accountNumber: bankToAdd.accountNumber,
+          routingNumber: bankToAdd.routingNumber,
+        },
       },
     });
     const {
@@ -131,26 +133,26 @@ test.describe("graphql playground", () => {
   });
 
   test("bank account creation and deletion", async ({ authenticatedRequest: request }) => {
-    const bankToAdd = {
-      bankName: "creation and deletion",
-      accountNumber: "123456789",
-      routingNumber: "0987654321",
-    };
     let res = await request.post(API_GRAPHQL, {
       data: {
-        query: `mutation {
+        query: `mutation createBankAccount ($bankName: String!, $accountNumber: String!, $routingNumber: String!) {
           createBankAccount(
-            bankName: "${bankToAdd.bankName}"
-            accountNumber: "${bankToAdd.accountNumber}"
-            routingNumber: "${bankToAdd.routingNumber}"
+            bankName: $bankName,
+            accountNumber: $accountNumber,
+            routingNumber: $routingNumber,
           ) {
             id  
+            bankName
           }
         }
         `,
+        variables: {
+          bankName: `${faker.company.companyName()} Bank`,
+          accountNumber: faker.finance.account(10),
+          routingNumber: faker.finance.account(9),
+        },
       },
     });
-    console.log(await res.json());
     const {
       data: { createBankAccount: newBankAccount },
       errors: newBankAccountErrors,
@@ -174,6 +176,126 @@ test.describe("graphql playground", () => {
     } = (await res.json()) as { data: { listBankAccount: BankAccount[] }; errors: any[] };
     expect(res.status()).toEqual(200);
     expect(queriedBankAccountErrors).toBeUndefined();
-    expect(queriedBankAccounts.find((ba) => ba.id === newBankAccount.id)).toBeDefined();
+    expect(queriedBankAccounts.length).toBeGreaterThan(1);
+    expect(queriedBankAccounts.at(-1)!.id).toEqual(newBankAccount.id);
+    expect(queriedBankAccounts.at(-1)!.bankName).toEqual(newBankAccount.bankName);
+
+    res = await request.post(API_GRAPHQL, {
+      data: {
+        query: `mutation {
+          deleteBankAccount(id: "${newBankAccount.id}")
+        }`,
+      },
+    });
+    expect(res.status()).toEqual(200);
+    const body = await res.json();
+    expect(body.data.deleteBankAccount).toBe(true);
+
+    res = await request.post(API_GRAPHQL, {
+      data: {
+        query: `query {
+          listBankAccount {
+            id
+            bankName
+            isDeleted
+          }
+        }`,
+      },
+    });
+    expect(res.status()).toBe(200);
+    const {
+      data: { listBankAccount: finalBankAccounts },
+      errors,
+    } = (await res.json()) as { data: { listBankAccount: BankAccount[] }; errors: any[] };
+    expect(errors).toBeUndefined();
+    expect(finalBankAccounts.at(-1)!.isDeleted).toBe(true);
+  });
+
+  test("missing required arg", async ({ authenticatedRequest: request }) => {
+    let res = await request.post(API_GRAPHQL, {
+      data: {
+        query: `mutation createBankAccount($bankName: String!, $accountNumber: String!, $routingNumber: String!) {
+          createBankAccount(bankName: $bankName, accountNumber: $accountNumber, routingNumber: $routingNumber) {
+            id
+          }
+        }`,
+        variables: {
+          accountNumber: faker.finance.account(10),
+          routingNumber: faker.finance.account(9),
+        },
+      },
+    });
+    expect(res.status()).toBe(200);
+    const { errors } = (await res.json()) as { errors: any[] };
+    expect(errors).toBeDefined();
+    expect(errors[0].message).toBe(
+      'Variable "$bankName" of required type "String!" was not provided.'
+    );
+  });
+
+  test("invalid variable type", async ({ authenticatedRequest: request }) => {
+    const res = await request.post(API_GRAPHQL, {
+      data: {
+        query: `mutation createBankAccount($bankName: String!, $accountNumber: String!, $routingNumber: String!) {
+          createBankAccount(bankName: $bankName, accountNumber: $accountNumber, routingNumber: $routingNumber) {
+            id
+          }
+        }`,
+        variables: {
+          bankName: `${faker.company.companyName()} bank`,
+          accountNumber: 1234567890,
+          routingNumber: faker.finance.account(9),
+        },
+      },
+    });
+    expect(res.status()).toBe(200);
+    const { errors } = (await res.json()) as { errors: any[] };
+    expect(errors[0].message).toBe(
+      'Variable "$accountNumber" got invalid value 1234567890; String cannot represent a non string value: 1234567890'
+    );
+  });
+
+  test("delete non-existant id", async ({ authenticatedRequest: request }) => {
+    const res = await request.post(API_GRAPHQL, {
+      data: {
+        query: `mutation deleteBankAccount($id: ID!) {
+          deleteBankAccount(id: $id)
+        }`,
+        variables: {
+          id: faker.random.alphaNumeric(11),
+        },
+      },
+    });
+    const {
+      data: { deleteBankAccount },
+    } = (await res.json()) as { data: { deleteBankAccount: boolean } };
+    expect(deleteBankAccount).toBe(true);
+  });
+
+  test("malformed query syntax", async ({ authenticatedRequest: request }) => {
+    const res = await request.post(API_GRAPHQL, {
+      data: {
+        query: `mutation createBankAccount {
+        }`,
+      },
+    });
+    expect(res.status()).toBe(200);
+    const { errors } = (await res.json()) as { errors: any[] };
+    expect(errors[0].message).toBe('Syntax Error: Expected Name, found "}".');
+  });
+
+  test("introspection query on BankAccount type", async ({ authenticatedRequest: request }) => {
+    const res = await request.post(API_GRAPHQL, {
+      data: {
+        query: `query {
+          __schema {
+            queryType { name fields { name args { name }} }
+            mutationType { name fields { name args { name }} }
+            types { name kind }
+          }
+        }`,
+      },
+    });
+    console.log(JSON.stringify(await res.json(), null, 2))
   });
 });
